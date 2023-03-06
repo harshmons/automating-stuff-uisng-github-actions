21deb090e7809977bc3cb5bdee16fd76991d91ba
goofydevs0429 <127080349+goofydevs0429@users.noreply.github.com>
2023-03-06 19:11:29 +0530

    Update index.spec.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
 import { isEven } from ".";
 
 it("should return true for even numbers",()=>{
-    expect(isEven(4)).toBeTruthy();
+    expect(isEven(4)).not.toBeTruthy();
 })
 
 it("should return false for odd numbers",()=>{
